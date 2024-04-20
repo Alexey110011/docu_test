1d1cc2ac8f03e5efef2a0be07b9afd8858b24dcb
Alexey <alleshkvch@gmail.com>
2024-04-20 09:44:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as allIcons from '@fortawesome/free-solid-svg-icons'
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
+
+import { findIconDefinition, IconDefinition, icon, library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'docu_test';
+  allIcons = allIcons                //Assigning all Awesome Fonts library to variable "allIcons"
+  faCoffee:IconDefinition = faCoffee //Default icon
+  iconNamesArray:string[]=[]     
+  title_icon:string = 'coffee'       //Empty array for filling with icon names
+  counter = 0
+  makeIconArray = ()=>{              //Method which fill empty "iconNamesArray" with icon names
+    for (let prop in this.allIcons){
+      this.iconNamesArray.push(prop)
+      console.log("This iconNamesArray",this.iconNamesArray)
+    }
+  } 
+  delayTwo(icons:any){
+    setTimeout(() => {
+      this.showIcon(icons)
+    }, 2000);
+  }
+   showIcon(icons:any){
+     this.counter++
+     let randomNumber = Math.floor(Math.random()*this.iconNamesArray.length) //Choose random index in iconNamesArray
+     let randomIcon = this.iconNamesArray[randomNumber]                      //Choose random icon name by above index
+     let i = findIconDefinition({prefix:'fas',iconName:icons[`${randomIcon}`].iconName})
+     let newIcon =  icon(i)
+     this.faCoffee = newIcon
+     this.title_icon = newIcon.iconName
+    }
+    constructor(){} 
+        ngOnInit():void {                                                    //Calling function  creating array of icon names
+           this.makeIconArray() 
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
